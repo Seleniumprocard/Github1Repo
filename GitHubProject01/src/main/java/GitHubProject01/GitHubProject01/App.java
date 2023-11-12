892c475b037e8f8a2cad27fbc7d105b412703590
Seleniumprocard <wehatemails@gmail.com>
2023-11-12 09:19:57 +0530

    First Commit

---
+++ GitHubProject01/src/main/java/GitHubProject01/GitHubProject01/App.java
@@ -0,0 +1,13 @@
+package GitHubProject01.GitHubProject01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
